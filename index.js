3d56f6cd82beb2d01f2c56eeddae757dae97e333
crystalll14 <nkemeh.crystal@stu.cu.ed.ng>
2023-02-27 06:30:51 +0100

    added js file

---
+++ index.js
@@ -0,0 +1,16 @@
+//Number 1
+
+function InstagramPost() {
+    this.handleofauthor = handleofauthor;
+    this.content = content;
+    this.imagelink = imagelink;
+    this.numberofviews = numberofviews;
+    this.numberoflikes = numberoflikes;
+}
+
+const Post1 = new InstagramPost ('jordanofyhwh', 'content','https://www.instagram.com/reel/CoI2cvrJUrL/?igshid=YmMyMTA2M2Y=',
+18000,132000)
+const Post2 = new InstagramPost ('bellaexplores', 'content','https://www.instagram.com/reel/CneTQC1pXCG/?igshid=YmMyMTA2M2Y=',
+17000,14600)
+console.log(Post1)
+console.log(Post2)
